60204111346be9adb9f029a2f407e85350ea5284
Sharik <sharikofficial99@gmail.com>
2023-07-11 23:39:42 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import './App.css'
+import Campanies from "./Components/Companies/Campanies";
+import Residencies from "./Components/Residencies/Residencies";
+import Value from "./Components/Value/Value";
+import Contact from "./Components/Contact/Contact";
+import Getstarted from "./Components/Getstarted/Getstarted";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header/>
+        <Hero/>
+      </div>
+      <Campanies/>
+      <Residencies/>
+      <Value/>
+      <Contact/>
+      <Getstarted/>
+      <Footer/>
+    </div>
   );
 }
 
---
+++ src/Components/Getstarted/Getstarted.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Getstarted.css'
+
+const Getstarted = () => {
+  return (
+    <section className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get Started with Homyz</span>
+                <span className='secondaryText'>Subscribe and find super attractive price quotes from homyz <br /> find your residence soon</span>
+                <button className="button">
+                    <a href="mailto:sharikofficial99@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
+  ) 
+}
+
+export default Getstarted
